20fa80b43d460c40d20c04dd7a884e60b96ddc11
Sumeet Agarwal <sumeet.a@gmail.com>
2021-09-07 16:05:19 -0700

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,7 @@ import Control.Monad.Except (MonadError, throwError)
 import Control.Monad.State (MonadState (get, put), State)
 import Data.Array (Array, Ix (inRange, index, range), (!), (//))
 import Data.BitSyntax (ReadType (Unsigned), bitSyn)
-import Data.Bits (Bits, shiftL, shiftR, (.&.))
+import Data.Bits (Bits, shiftL, shiftR, xor, (.&.), (.|.))
 import qualified Data.ByteString as BS
 import Data.Word (Word16, Word8)
 import Data.Word.Odd (Word4)
@@ -36,8 +36,14 @@ joinNibbles ns = sum $ zipWith (*) (map fromIntegral $ reverse ns) nibbleMasks
   where
     nibbleMasks = iterate (`shiftL` 4) 1
 
-chop16 :: (Word8, Word8) -> (Word4, Word4, Word4, Word4)
-chop16 (l, r) = (ll, lr, rl, rr)
+chop16 :: Word16 -> (Word8, Word8)
+chop16 w = (fromIntegral l, fromIntegral r)
+  where
+    l = (w `shiftR` 8) .&. 0xff
+    r = w .&. 0xff
+
+chop8s :: (Word8, Word8) -> (Word4, Word4, Word4, Word4)
+chop8s (l, r) = (ll, lr, rl, rr)
   where
     (ll, lr) = chop8 l
     (rl, rr) = chop8 r
@@ -73,21 +79,64 @@ interp comp@Computer {memory, stack, pc, registers} = case inst of
     let byte = joinNibbles [bl, bh]
      in Right $ if registers ! vx /= byte then skipComp else nextComp
   -- SE Vx, Vy: Skip next instruction if Vx == Vy
-  (0x5, vx, vy, 0) ->
+  (0x5, vx, vy, 0x0) ->
     Right $ if registers ! vx /= registers ! vy then skipComp else nextComp
   -- LD Vx, byte: Set Vx = byte
   (0x6, vx, bl, bh) ->
     let byte = joinNibbles [bl, bh]
-     in Right $ comp {pc = nextPc, registers = registers // [(vx, byte)]}
+     in Right $ nextComp {registers = registers // [(vx, byte)]}
+  -- ADD Vx, byte: Set Vx = Vx + byte
+  (0x7, vx, bl, bh) ->
+    let byte = joinNibbles [bl, bh]
+        vx' = (registers ! vx) + byte
+     in Right $ nextComp {registers = registers // [(vx, vx')]}
+  -- LD Vx, Vy: Set Vx = Vy
+  (0x8, vx, vy, 0x0) ->
+    Right $ nextComp {registers = registers // [(vx, registers ! vy)]}
+  -- OR Vx, Vy: Set Vx = Vx OR Vy (bitwise OR)
+  (0x8, vx, vy, 0x1) ->
+    let vx' = (registers ! vx) .|. (registers ! vy)
+     in Right $ nextComp {registers = registers // [(vx, vx')]}
+  -- AND Vx, Vy: Set Vx = Vx OR Vy (bitwise AND)
+  (0x8, vx, vy, 0x2) ->
+    let vx' = (registers ! vx) .&. (registers ! vy)
+     in Right $ nextComp {registers = registers // [(vx, vx')]}
+  -- XOR Vx, Vy: Set Vx = Vx XOR Vy (bitwise XOR)
+  (0x8, vx, vy, 0x3) ->
+    let vx' = (registers ! vx) `xor` (registers ! vy)
+     in Right $ nextComp {registers = registers // [(vx, vx')]}
+  -- ADD Vx, Vy: Set Vx = Vx + Vy, set VF = carry
+  (0x8, vx, vy, 0x4) -> Right $ nextComp {registers = registers'}
+    where
+      registers' = registers // [(vx, sum), (vf, if carry then 0x1 else 0x0)]
+      (sum, carry) = add (registers ! vx) (registers ! vy)
+  -- SUB Vx, Vy: Set Vx = Vx - Vy, set VF = NOT borrow
+  (0x8, vx, vy, 0x5) -> Right $ nextComp {registers = registers'}
+    where
+      registers' = registers // [(vx, diff), (vf, if valX > valY then 0x1 else 0x0)]
+      diff = valX - valY
+      (valX, valY) = (registers ! vx, registers ! vy)
+  -- SHR Vx: Set Vy SHR 1, set VF = LSB Vy
+  (0x8, vx, vy, 0x6) -> Right $ nextComp {registers = registers'}
+    where
+      (valX, valY) = (registers ! vx, registers ! vy)
+      registers' = registers // [(vx, valY `shiftR` 1), (vf, valY .&. 0b1)]
+
   -- Error: Invalid instruction
   notfound -> Left $ "invalid instruction " <> show notfound
   where
-    inst = chop16 (memory ! pc, memory ! pc + 1)
+    inst = chop8s (memory ! pc, memory ! pc + 1)
     nextPc = pc + 1
     nextComp = comp {pc = nextPc}
     skipPc = nextPc + 1
     skipComp = comp {pc = skipPc}
     getReg = (registers !)
+    vf = 0xf
+
+add :: Word8 -> Word8 -> (Word8, Bool)
+add x y = (sumL, sumH /= 0x0)
+  where
+    (sumH, sumL) = chop16 $ fromIntegral x + fromIntegral y
 
 data Computer = Computer
   { -- usually 4096 addresses from 0x000 to 0xFFF
