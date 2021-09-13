ed1c71d1364706585c9d8c9c71c84f21e4bd7516
Sumeet Agarwal <sumeet.a@gmail.com>
2021-09-13 12:30:03 -0700

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -18,6 +18,7 @@ import Data.Word (Word16, Word32, Word8)
 import Data.Word.Odd (Word4)
 import Data.Word12 (Word12)
 import Font (hexFont)
+import GHC.Arr (Array (Array))
 import System.Random (StdGen, genWord8)
 
 -- from https://hackage.haskell.org/package/base-4.13.0.0/docs/src/GHC.Word.html
@@ -69,152 +70,182 @@ timing newNumMsSinceOn comp@Computer {delayTimer, soundTimer, numMsSinceOn} =
     numTicksLastTime = fromIntegral numMsSinceOn / ((1 / 60) * 1000)
 
 interp :: Computer -> Either String Computer
-interp comp@Computer {memory, stack, pc, registers, randGen, delayTimer, iReg} = case inst of
-  -- CLS: Clear the display
-  (0x0, 0x0, 0xe, 0x0) -> Right $ nextComp {display = clearDisplay}
-  -- RET: Return from a subroutine
-  (0x0, 0x0, 0xe, 0xe) -> do
-    (newStack, retAddr) <- maybeToEither "stack underflow" $ unsnoc stack
-    return comp {stack = newStack, pc = retAddr}
-  -- SYS addr: Jump to a machine code routine at addr
-  (0x0, nl, nm, nr) -> Right $ comp {pc = joinNibbles [nl, nm, nr]}
-  -- JP addr: Jump to addr
-  (0x1, nl, nm, nr) -> Right $ comp {pc = joinNibbles [nl, nm, nr]}
-  -- CALL addr: Call subroutine at addr
-  (0x2, nl, nm, nr) ->
-    Right $
-      comp {stack = stack ++ [nextPc], pc = joinNibbles [nl, nm, nr]}
-  -- SE Vx, byte: Skip next instruction if Vx == byte
-  (0x3, vx, bl, bh) ->
-    let byte = joinNibbles [bl, bh]
-     in Right $ if registers ! vx == byte then skipComp else nextComp
-  -- SNE Vx, byte: Skip next instruction if Vx != byte
-  (0x4, vx, bl, bh) ->
-    let byte = joinNibbles [bl, bh]
-     in Right $ if registers ! vx /= byte then skipComp else nextComp
-  -- SE Vx, Vy: Skip next instruction if Vx == Vy
-  (0x5, vx, vy, 0x0) ->
-    Right $ if registers ! vx /= registers ! vy then skipComp else nextComp
-  -- LD Vx, byte: Set Vx = byte
-  (0x6, vx, bl, bh) ->
-    let byte = joinNibbles [bl, bh]
-     in Right $ nextComp {registers = registers // [(vx, byte)]}
-  -- ADD Vx, byte: Set Vx = Vx + byte
-  (0x7, vx, bl, bh) ->
-    let byte = joinNibbles [bl, bh]
-        vx' = (registers ! vx) + byte
-     in Right $ nextComp {registers = registers // [(vx, vx')]}
-  -- LD Vx, Vy: Set Vx = Vy
-  (0x8, vx, vy, 0x0) ->
-    Right $ nextComp {registers = registers // [(vx, registers ! vy)]}
-  -- OR Vx, Vy: Set Vx = Vx OR Vy (bitwise OR)
-  (0x8, vx, vy, 0x1) ->
-    let vx' = (registers ! vx) .|. (registers ! vy)
-     in Right $ nextComp {registers = registers // [(vx, vx')]}
-  -- AND Vx, Vy: Set Vx = Vx OR Vy (bitwise AND)
-  (0x8, vx, vy, 0x2) ->
-    let vx' = (registers ! vx) .&. (registers ! vy)
-     in Right $ nextComp {registers = registers // [(vx, vx')]}
-  -- XOR Vx, Vy: Set Vx = Vx XOR Vy (bitwise XOR)
-  (0x8, vx, vy, 0x3) ->
-    let vx' = (registers ! vx) `xor` (registers ! vy)
-     in Right $ nextComp {registers = registers // [(vx, vx')]}
-  -- ADD Vx, Vy: Set Vx = Vx + Vy, set VF = carry
-  (0x8, vx, vy, 0x4) -> Right $ nextComp {registers = registers'}
+interp
+  comp@Computer
+    { memory,
+      stack,
+      pc,
+      registers,
+      randGen,
+      delayTimer,
+      iReg,
+      display
+    } = case inst of
+    -- CLS: Clear the display
+    (0x0, 0x0, 0xe, 0x0) -> Right $ nextComp {display = clearDisplay}
+    -- RET: Return from a subroutine
+    (0x0, 0x0, 0xe, 0xe) -> do
+      (newStack, retAddr) <- maybeToEither "stack underflow" $ unsnoc stack
+      return comp {stack = newStack, pc = retAddr}
+    -- SYS addr: Jump to a machine code routine at addr
+    (0x0, nl, nm, nr) -> Right $ comp {pc = joinNibbles [nl, nm, nr]}
+    -- JP addr: Jump to addr
+    (0x1, nl, nm, nr) -> Right $ comp {pc = joinNibbles [nl, nm, nr]}
+    -- CALL addr: Call subroutine at addr
+    (0x2, nl, nm, nr) ->
+      Right $
+        comp {stack = stack ++ [nextPc], pc = joinNibbles [nl, nm, nr]}
+    -- SE Vx, byte: Skip next instruction if Vx == byte
+    (0x3, vx, bl, bh) ->
+      let byte = joinNibbles [bl, bh]
+       in Right $ if registers ! vx == byte then skipComp else nextComp
+    -- SNE Vx, byte: Skip next instruction if Vx != byte
+    (0x4, vx, bl, bh) ->
+      let byte = joinNibbles [bl, bh]
+       in Right $ if registers ! vx /= byte then skipComp else nextComp
+    -- SE Vx, Vy: Skip next instruction if Vx == Vy
+    (0x5, vx, vy, 0x0) ->
+      Right $ if registers ! vx /= registers ! vy then skipComp else nextComp
+    -- LD Vx, byte: Set Vx = byte
+    (0x6, vx, bl, bh) ->
+      let byte = joinNibbles [bl, bh]
+       in Right $ nextComp {registers = registers // [(vx, byte)]}
+    -- ADD Vx, byte: Set Vx = Vx + byte
+    (0x7, vx, bl, bh) ->
+      let byte = joinNibbles [bl, bh]
+          vx' = (registers ! vx) + byte
+       in Right $ nextComp {registers = registers // [(vx, vx')]}
+    -- LD Vx, Vy: Set Vx = Vy
+    (0x8, vx, vy, 0x0) ->
+      Right $ nextComp {registers = registers // [(vx, registers ! vy)]}
+    -- OR Vx, Vy: Set Vx = Vx OR Vy (bitwise OR)
+    (0x8, vx, vy, 0x1) ->
+      let vx' = (registers ! vx) .|. (registers ! vy)
+       in Right $ nextComp {registers = registers // [(vx, vx')]}
+    -- AND Vx, Vy: Set Vx = Vx OR Vy (bitwise AND)
+    (0x8, vx, vy, 0x2) ->
+      let vx' = (registers ! vx) .&. (registers ! vy)
+       in Right $ nextComp {registers = registers // [(vx, vx')]}
+    -- XOR Vx, Vy: Set Vx = Vx XOR Vy (bitwise XOR)
+    (0x8, vx, vy, 0x3) ->
+      let vx' = (registers ! vx) `xor` (registers ! vy)
+       in Right $ nextComp {registers = registers // [(vx, vx')]}
+    -- ADD Vx, Vy: Set Vx = Vx + Vy, set VF = carry
+    (0x8, vx, vy, 0x4) -> Right $ nextComp {registers = registers'}
+      where
+        registers' = registers // [(vx, sum), (vf, if carry then 0x1 else 0x0)]
+        (sum, carry) = add (registers ! vx) (registers ! vy)
+    -- SUB Vx, Vy: Set Vx = Vx - Vy, set VF = NOT borrow
+    (0x8, vx, vy, 0x5) -> Right $ nextComp {registers = registers'}
+      where
+        registers' = registers // [(vx, diff), (vf, if valX > valY then 0x1 else 0x0)]
+        diff = valX - valY
+        (valX, valY) = (registers ! vx, registers ! vy)
+    -- SHR Vx: Set Vy SHR 1, set VF = 1 if LSB Vy
+    (0x8, vx, vy, 0x6) -> Right $ nextComp {registers = registers'}
+      where
+        (valX, valY) = (registers ! vx, registers ! vy)
+        registers' = registers // [(vx, valY `shiftR` 1), (vf, valY .&. 0b1)]
+    -- SUBN Vx, Vy: Set Vx = Vy - Vx, set VF = NOT borrow
+    (0x8, vx, vy, 0x7) -> Right $ nextComp {registers = registers'}
+      where
+        registers' = registers // [(vx, diff), (vf, if valY > valX then 0x1 else 0x0)]
+        diff = valY - valX
+        (valX, valY) = (registers ! vx, registers ! vy)
+    -- SHL Vx: Set Vy SHR 1, set VF = 1 if MSB Vy
+    (0x8, vx, vy, 0xe) -> Right $ nextComp {registers = registers'}
+      where
+        (valX, valY) = (registers ! vx, registers ! vy)
+        msbIsSet = valY .&. 0b10000000 /= 0
+        registers' = registers // [(vx, valY `shiftL` 1), (vf, if msbIsSet then 1 else 0)]
+    -- SNE Vx, Vy: Skip next instruction if Vx != Vy
+    (0x9, vx, vy, 0) ->
+      Right $ if registers ! vx /= registers ! vy then skipComp else nextComp
+    -- LD nnn: Set I = nnn
+    (0xa, nl, nm, nr) -> Right $ nextComp {iReg = joinNibbles [nl, nm, nr]}
+    -- JP V0, addr: Jump to location nnn + V0
+    (0xb, nl, nm, nr) -> Right $ comp {pc = jumpDest}
+      where
+        jumpDest = joinNibbles [nl, nm, nr] + fromIntegral (registers ! v0)
+    -- RND Vx, byte: Set Vx = random byte AND kk
+    (0xc, vx, nl, nh) -> Right $ nextComp {randGen = randGen', registers = registers'}
+      where
+        (randByte, randGen') = genWord8 randGen
+        registers' = registers // [(vx, randByte .&. joinNibbles [nl, nh])]
+    -- DRW Vx, Vy, nibble: Display n-byte sprite starting at memory location I at
+    -- (Vx, Vy), set VF = collision
+    -- sprites are always 8 across
+    (0xd, vx, vy, n) -> Right $ nextComp {display = display // updates}
+      where
+        updates = undefined
+        startX = registers ! vx
+        startY = registers ! vy
+    -- SKP Vx: Skip next instruction if key with the value of Vx is pressed
+    -- TODO: unimplemented until we have concept of keyboard
+    (0xe, vx, 0x9, 0xe) -> undefined
+    -- SKNP Vx: Skip next instruction if key with the value of Vx is not
+    -- pressed
+    -- TODO: unimplemented until we have concept of keyboard
+    (0xe, vx, 0xa, 0x1) -> undefined
+    -- Fx07 - LD Vx, DT: Set Vx = delay timer value.
+    (0xf, vx, 0x0, 0x7) -> Right $ nextComp {registers = registers // [(vx, delayTimer)]}
+    -- LD Vx, K: Wait for a key press, store the value of the key in Vx
+    -- TODO: unimplemented until we have a concept of keyboard and concept of waiting
+    (0xf, vx, 0x0, 0xa) -> undefined
+    -- LD DT, Vx: Set delay timer = Vx
+    (0xf, vx, 0x1, 0x5) -> Right $ nextComp {delayTimer = registers ! vx}
+    -- LD ST, Vx: Set sound timer = Vx
+    (0xf, vx, 0x1, 0x8) -> Right $ nextComp {soundTimer = registers ! vx}
+    -- ADD I, Vx: Set I = I + Vx
+    (0xf, vx, 0x1, 0xe) ->
+      Right $
+        nextComp
+          { iReg =
+              iReg + fromIntegral (registers ! vx)
+          }
+    -- LD F, Vx: Set I = location of sprite for digit Vx
+    -- TODO: unimplemented until we load the font into memory
+    (0xf, vx, 0x2, 0x9) -> undefined
+    -- LD B, Vx: Store BCD representation of Vx in memory locations
+    -- I, I+1, and I+2:
+    -- Hundreds digit in memory at location in I, the tens digit at location I+1,
+    -- and the ones digit at location I+2
+    (0xf, vx, 0x3, 0x3) -> Right $ nextComp {memory = memory // updates}
+      where
+        memoryLocations = take 3 $ countUpFrom iReg
+        updates = zip memoryLocations (bcd3 $ registers ! vx)
+    -- LD [I], Vx: Store registers V0 through Vx in memory starting at location I
+    (0xf, vx, 0x5, 0x5) -> Right $ nextComp {memory = memory // updates}
+      where
+        updates =
+          zip (countUpFrom iReg) $
+            map (registers !) [fromIntegral v0 .. vx]
+    -- LD Vx, [I]: Read registers V0 through Vx from memory starting at location I.
+    (0xf, vx, 0x6, 0x5) -> Right $ nextComp {registers = registers // updates}
+      where
+        updates =
+          zip [fromIntegral v0 .. vx] $
+            map (memory !) $ countUpFrom iReg
+    --
+    -- END OF INSTRUCTIONS
+    -- Error: Invalid instruction
+    notfound -> Left $ "invalid instruction " <> show notfound
     where
-      registers' = registers // [(vx, sum), (vf, if carry then 0x1 else 0x0)]
-      (sum, carry) = add (registers ! vx) (registers ! vy)
-  -- SUB Vx, Vy: Set Vx = Vx - Vy, set VF = NOT borrow
-  (0x8, vx, vy, 0x5) -> Right $ nextComp {registers = registers'}
-    where
-      registers' = registers // [(vx, diff), (vf, if valX > valY then 0x1 else 0x0)]
-      diff = valX - valY
-      (valX, valY) = (registers ! vx, registers ! vy)
-  -- SHR Vx: Set Vy SHR 1, set VF = 1 if LSB Vy
-  (0x8, vx, vy, 0x6) -> Right $ nextComp {registers = registers'}
-    where
-      (valX, valY) = (registers ! vx, registers ! vy)
-      registers' = registers // [(vx, valY `shiftR` 1), (vf, valY .&. 0b1)]
-  -- SUBN Vx, Vy: Set Vx = Vy - Vx, set VF = NOT borrow
-  (0x8, vx, vy, 0x7) -> Right $ nextComp {registers = registers'}
-    where
-      registers' = registers // [(vx, diff), (vf, if valY > valX then 0x1 else 0x0)]
-      diff = valY - valX
-      (valX, valY) = (registers ! vx, registers ! vy)
-  -- SHL Vx: Set Vy SHR 1, set VF = 1 if MSB Vy
-  (0x8, vx, vy, 0xe) -> Right $ nextComp {registers = registers'}
-    where
-      (valX, valY) = (registers ! vx, registers ! vy)
-      msbIsSet = valY .&. 0b10000000 /= 0
-      registers' = registers // [(vx, valY `shiftL` 1), (vf, if msbIsSet then 1 else 0)]
-  -- SNE Vx, Vy: Skip next instruction if Vx != Vy
-  (0x9, vx, vy, 0) ->
-    Right $ if registers ! vx /= registers ! vy then skipComp else nextComp
-  -- LD nnn: Set I = nnn
-  (0xa, nl, nm, nr) -> Right $ nextComp {iReg = joinNibbles [nl, nm, nr]}
-  -- JP V0, addr: Jump to location nnn + V0
-  (0xb, nl, nm, nr) -> Right $ comp {pc = jumpDest}
-    where
-      jumpDest = joinNibbles [nl, nm, nr] + fromIntegral (registers ! v0)
-  -- RND Vx, byte: Set Vx = random byte AND kk
-  (0xc, vx, nl, nh) -> Right $ nextComp {randGen = randGen', registers = registers'}
-    where
-      (randByte, randGen') = genWord8 randGen
-      registers' = registers // [(vx, randByte .&. joinNibbles [nl, nh])]
-  -- DRW Vx, Vy, nibble: Display n-byte sprite starting at memory location I at
-  -- (Vx, Vy), set VF = collision
-  -- TODO: unimplemented until we have concept of display
-  (0xd, vx, vy, n) -> undefined
-  -- SKP Vx: Skip next instruction if key with the value of Vx is pressed
-  -- TODO: unimplemented until we have concept of keyboard
-  (0xe, vx, 0x9, 0xe) -> undefined
-  -- SKNP Vx: Skip next instruction if key with the value of Vx is not
-  -- pressed
-  -- TODO: unimplemented until we have concept of keyboard
-  (0xe, vx, 0xa, 0x1) -> undefined
-  -- Fx07 - LD Vx, DT: Set Vx = delay timer value.
-  (0xf, vx, 0x0, 0x7) -> Right $ nextComp {registers = registers // [(vx, delayTimer)]}
-  -- LD Vx, K: Wait for a key press, store the value of the key in Vx
-  -- TODO: unimplemented until we have a concept of keyboard and concept of waiting
-  (0xf, vx, 0x0, 0xa) -> undefined
-  -- LD DT, Vx: Set delay timer = Vx
-  (0xf, vx, 0x1, 0x5) -> Right $ nextComp {delayTimer = registers ! vx}
-  -- LD ST, Vx: Set sound timer = Vx
-  (0xf, vx, 0x1, 0x8) -> Right $ nextComp {soundTimer = registers ! vx}
-  -- ADD I, Vx: Set I = I + Vx
-  (0xf, vx, 0x1, 0xe) -> Right $ nextComp {iReg = iReg + fromIntegral (registers ! vx)}
-  -- LD F, Vx: Set I = location of sprite for digit Vx
-  -- TODO: unimplemented until we load the font into memory
-  (0xf, vx, 0x2, 0x9) -> undefined
-  -- LD B, Vx: Store BCD representation of Vx in memory locations
-  -- I, I+1, and I+2:
-  -- Hundreds digit in memory at location in I, the tens digit at location I+1,
-  -- and the ones digit at location I+2
-  (0xf, vx, 0x3, 0x3) -> Right $ nextComp {memory = memory // updates}
-    where
-      memoryLocations = take 3 $ countUpFrom iReg
-      updates = zip memoryLocations (bcd3 $ registers ! vx)
-  -- LD [I], Vx: Store registers V0 through Vx in memory starting at location I
-  (0xf, vx, 0x5, 0x5) -> Right $ nextComp {memory = memory // updates}
-    where
-      updates = zip (countUpFrom iReg) $ map (registers !) [fromIntegral v0 .. vx]
-  -- LD Vx, [I]: Read registers V0 through Vx from memory starting at location I.
-  (0xf, vx, 0x6, 0x5) -> Right $ nextComp {registers = registers // updates}
-    where
-      updates = zip [fromIntegral v0 .. vx] $ map (memory !) $ countUpFrom iReg
-  --
-  -- END OF INSTRUCTIONS
-  -- Error: Invalid instruction
-  notfound -> Left $ "invalid instruction " <> show notfound
+      inst = chop8s (memory ! pc, memory ! pc + 1)
+      nextPc = pc + 1
+      nextComp = comp {pc = nextPc}
+      skipPc = nextPc + 1
+      skipComp = comp {pc = skipPc}
+      getReg = (registers !)
+      vf = 0xf
+      v0 = 0x0
+
+drawRange :: Display -> Word8 -> Word8 -> Int -> [(Word8, Word8)]
+drawRange (Array _ (maxX, maxY) _ _) startX startY n =
+  [(x, y) | y <- ys, x <- xs]
   where
-    inst = chop8s (memory ! pc, memory ! pc + 1)
-    nextPc = pc + 1
-    nextComp = comp {pc = nextPc}
-    skipPc = nextPc + 1
-    skipComp = comp {pc = skipPc}
-    getReg = (registers !)
-    vf = 0xf
-    v0 = 0x0
+    ys = map (`mod` (maxY + 1)) [startY .. startY + fromIntegral n - 1]
+    xs = map (`mod` (maxX + 1)) [startX .. startX + 7]
 
 countUpFrom :: Integral i => i -> [i]
 countUpFrom = iterate (+ 1)
